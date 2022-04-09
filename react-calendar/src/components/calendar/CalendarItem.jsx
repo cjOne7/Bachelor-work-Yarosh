d4f3f24d9f4ff8589a1204f2c94a3f214bcbb845
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2022-04-10 00:27:37 +0200

    Added filters on calendar items

--- react-calendar/src/components/calendar/CalendarItem.jsx
+++ react-calendar/src/components/calendar/CalendarItem.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
 import {Form} from 'react-bootstrap';
 import './table.css';
+import moment from "moment";
 
 const CalendarItem = ({event}) => {
+    const checkForEmpty = value => value === '' ? '-' : value;
+    const dateFormatter = value => moment(value).format('DD.MM.YYYY HH:mm');
+    const trimLongString = value => {
+        const MIN_DISPLAYED_LENGTH = 20;
+        return value.length >= MIN_DISPLAYED_LENGTH ? value.slice(0, minDisplayedLength).trim() + "..." : value;
+    };
     return (
         <tr>
             <td>
                 <Form.Check type={'checkbox'}/>
             </td>
             <td title={event.subject}>
-                {event.subject}
+                {trimLongString(event.subject)}
             </td>
             <td title={event.bodyPreview}>
-                {event.bodyPreview}
+                {trimLongString(checkForEmpty(event.bodyPreview))}
             </td>
             <td title={event.organizer.emailAddress.name}>
-                {event.organizer.emailAddress.name}
+                {trimLongString(event.organizer.emailAddress.name)}
             </td>
             <td title={event.location.displayName}>
-                {event.location.displayName}
+                {trimLongString(checkForEmpty(event.location.displayName))}
             </td>
             <td title={event.start.dateTime}>
-                {event.start.dateTime}
+                {dateFormatter(event.start.dateTime)}
             </td>
         </tr>
     );
