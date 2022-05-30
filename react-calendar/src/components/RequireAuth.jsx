c48ae403ceee6e384c39416ff73bcbba4e1760d5
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2022-05-30 17:55:15 +0200

    fix

--- react-calendar/src/components/RequireAuth.jsx
+++ react-calendar/src/components/RequireAuth.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import {useLocation, Navigate} from "react-router-dom";
-import {useSelector} from "react-redux";
+import { useLocation, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-const RequireAuth = ({children}) => {
+const RequireAuth = ({ children }) => {
     const location = useLocation();
-    const isAuthenticated = useSelector(state => state.authReducer.isAuthenticated);
+    const isAuthenticated = useSelector(state =>
+        state.authReducer.isAuthenticated);
     if (!isAuthenticated) {
-        return <Navigate to={'/'} state={{from: location}} replace={true}/>
+        return <Navigate to={'/'} state={{ from: location }} replace={true} />
     }
     return children;
 };
-
-export default RequireAuth;
